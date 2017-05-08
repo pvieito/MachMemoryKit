3c81c1aa8afd322554f49e5ee0f5fbe3e97036e3
Pedro José Pereira Vieito <pvieito@gmail.com>
2017-05-08 14:30:24 +0200

    CommandLineKit instead of Commander module in MemoryTool

--- MemoryTool/main.swift
+++ MemoryTool/main.swift
@@ -8,61 +8,76 @@
 
 import Foundation
 import MachKit
-import Commander
+import CommandLineKit
 import LoggerKit
 
+
+let processOption = StringOption(shortFlag: "p", longFlag: "process", required: true, helpMessage: "Process name or PID.")
+let expectedMemoryOption = StringOption(shortFlag: "e", longFlag: "expected", helpMessage: "Expected Memory as an hex string.")
+let patchedMemoryOption = StringOption(shortFlag: "d", longFlag: "patched", helpMessage: "Patched Memory as an hex string.")
+let offsetAddressOption = StringOption(shortFlag: "a", longFlag: "address", helpMessage: "Memory offset address to patch.")
+let verboseOption = BoolOption(shortFlag: "v", longFlag: "verbose", helpMessage: "Verbose Mode.")
+let helpOption = BoolOption(shortFlag: "h", longFlag: "help", helpMessage: "Prints a help message.")
+
+let cli = CommandLineKit.CommandLine()
+cli.addOptions(processOption, expectedMemoryOption, patchedMemoryOption, offsetAddressOption, verboseOption, helpOption)
+
+do {
+    try cli.parse()
+} catch {
+    cli.printUsage(error)
+    exit(EX_USAGE)
+}
+
+if helpOption.value {
+    cli.printUsage()
+    exit(0)
+}
+
 Logger.logMode = .commandLine
+Logger.logLevel = verboseOption.value ? .debug : .info
 
-let processArgument = Argument<String>("process")
+if getuid() != 0 {
+    Logger.log(error: "You have to run this as root.")
+    exit(-1)
+}
 
-let expectedMemoryOption = Option("expected", "--", flag: "e", description: "Expected Memory as an hex string")
-let patchedMemoryOption = Option("patched", "--", flag: "p", description: "Patched Memory as an hex string")
-let offsetAddressOption = Option("address", "--", flag: "a", description: "Memory offset address to patch")
+do {
 
-let verboseFlag = Flag("verbose", flag: "v", description: "Verbose Mode", default: false)
+    guard let processString = processOption.value else {
+        Logger.log(error: "No process specified.")
+        exit(EX_USAGE)
+    }
 
-let main = command(processArgument, expectedMemoryOption, patchedMemoryOption, offsetAddressOption, verboseFlag) { processArgument, expectedMemory, patchedMemory, offsetAddressString, verbose in
+    var process: MachProcess
 
-    Logger.logLevel = verbose ? .debug : .info
+    if let pid = Int(processString) {
+        try process = MachProcess(pid: pid)
+    }
+    else {
+        try process = MachProcess(processName: processString)
+    }
 
-    if getuid() != 0 {
-        Logger.log(error: "You have to run this as root.")
+    Logger.log(important: "PID: \(process.pid)")
+
+    Logger.log(info: "ASLR Offset: \(process.memory.aslrOffset.hexString)")
+    Logger.log(info: "Base Address: \(process.memory.baseAddress.hexString)")
+
+    guard let expectedMemory = expectedMemoryOption.value, let patchedMemory = patchedMemoryOption.value, let offsetAddressString = offsetAddressOption.value else {
+        exit(0)
+    }
+
+    guard let offsetAddress = MachVirtualMemory.Address(hexString: offsetAddressString) else {
+        Logger.log(error: "Offset Address not valid.")
         exit(-1)
     }
 
-    do {
-        var process: MachProcess
+    let patchAddress = offsetAddress + process.memory.baseAddress
 
-        if let pid = Int(processArgument) {
-            try process = MachProcess(pid: pid)
-        }
-        else {
-            try process = MachProcess(processName: processArgument)
-        }
+    try process.memory.patch(expectedMemory, with: patchedMemory, on: patchAddress)
 
-        Logger.log(important: "PID: \(process.pid)")
-
-        Logger.log(info: "ASLR Offset: \(process.memory.aslrOffset.hexString)")
-        Logger.log(info: "Base Address: \(process.memory.baseAddress.hexString)")
-
-        guard expectedMemory != "--", patchedMemory != "--", offsetAddressString != "--" else {
-            exit(0)
-        }
-
-        guard let offsetAddress = MachVirtualMemory.Address(hexString: offsetAddressString) else {
-            Logger.log(error: "Offset Address not valid.")
-            exit(-1)
-        }
-
-        let patchAddress = offsetAddress + process.memory.baseAddress
-
-        try process.memory.patch(expectedMemory, with: patchedMemory, on: patchAddress)
-
-        Logger.log(success: "Memory correctly patched at \(patchAddress.hexString): \(expectedMemory) -> \(patchedMemory)")
-    }
-    catch {
-        Logger.log(error: error.localizedDescription)
-    }
+    Logger.log(success: "Memory correctly patched at \(patchAddress.hexString): \(expectedMemory) -> \(patchedMemory)")
 }
-
-main.run()
+catch {
+    Logger.log(error: error.localizedDescription)
+}
